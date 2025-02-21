f548f82a08469fdddeeb84566929a016143e7979
amare1990 <amaremek@gmail.com>
2025-02-21 13:10:13 +0300

    Implement functionality to an ARIMA model for the price data

--- scripts/statistical_modeling.py
+++ scripts/statistical_modeling.py
@@ -11,3 +11,9 @@ import pymc3 as pm
 class StatisticalModel:
     def __init__(self, data):
         self.data = data
+
+    def fit_arima(self, order=(1,1,1)):
+        """Fit an ARIMA model"""
+        model = ARIMA(self.data['Price'], order=order)
+        self.arima_result = model.fit()
+        print(self.arima_result.summary())
