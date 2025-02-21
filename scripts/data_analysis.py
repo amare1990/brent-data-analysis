6aca2420371d467c2b09146d45634e45c73aa3e4
amare1990 <amaremek@gmail.com>
2025-02-21 10:26:46 +0300

    Add visualizations method to plot price versus date

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -2,18 +2,22 @@
 
 import pandas as pd
 
+import matplotlib.pyplot as plt
+import seaborn as sns
+
 
 class BrentDataAnalysis:
     def __init__(self, file_path):
         """Initialize with data file path"""
         self.file_path = file_path
-        self.df = None
+        self.data = self.load_data()
+        # self.load_data = None
 
     def load_data(self):
         """Load Brent oil price dataset"""
-        self.df = pd.read_csv(self.file_path, parse_dates=["Date"], index_col="Date")
+        self.data = pd.read_csv(self.file_path, parse_dates=["Date"], index_col="Date")
         print("✅ Data loaded successfully!")
-        return self.df.head()
+        return self.data
 
     def overview_of_data(self):
         """Provide an overview of the dataset."""
@@ -27,5 +31,39 @@ class BrentDataAnalysis:
 
     def describe_data(self):
         """Provide basic summary statistics"""
+        print(f"{'*'*70}")
         print(f"Basic statistical summary for numerical columns\n {self.data.describe()}")
-        print(f"\nBasic statistical summary for categorical columns\n {self.data.describe(include=[object, 'category'])}")
+        if self.data.dtypes[self.data.dtypes == "object"].any():
+          print(f"\nBasic statistical summary for categorical columns\n {self.data.describe(include=[object, 'category'])}")
+        else:
+          print("No categorical or object columns found.")
+
+    def identify_missing_values(self):
+        """Identify missing values in the dataset."""
+        print(f"{'*'*70}")
+        print("Missing Values:")
+        missing_values = self.data.isnull().sum()
+        print(missing_values[missing_values > 0])
+        print("\n")
+
+    def remove_duplicates(self):
+        """Remove duplicate rows from the dataset."""
+        rows_before = self.data.shape[0]
+        self.data.drop_duplicates(inplace=True)
+        rows_after = self.data.shape[0]
+        print(f"{'*'*70}")
+        print("Duplicates removed.")
+        print(f"Shape of the data after removing duplicates. \n {self.data.shape}")
+        print(f"Number of rows removed for the duplicate rows case: {rows_before - rows_after}")
+        print("\n")
+
+    def plot_time_series(self):
+        """Plot time series data."""
+        plt.figure(figsize=(12, 6))
+        # plt.plot(self.data.index, self.data['Price'])
+        sns.lineplot(data=self.data, x=self.data.index, y='Price')
+        plt.title('Brent Oil Price Time Series')
+        plt.xlabel('Date')
+        plt.ylabel('Price')
+
+
