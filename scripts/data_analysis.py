1abb9c5314651e630a7847b75eb77673c324a7df
amare1990 <amaremek@gmail.com>
2025-02-21 11:44:33 +0300

    Create and run data analyzer notebook

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -16,11 +16,13 @@ class BrentDataAnalysis:
     def load_data(self):
         """Load Brent oil price dataset"""
         self.data = pd.read_csv(self.file_path, parse_dates=["Date"], index_col="Date")
+        print(f"\n{'*'*70}")
         print("✅ Data loaded successfully!")
         return self.data
 
     def overview_of_data(self):
         """Provide an overview of the dataset."""
+        print(f"\n{'*'*70}")
         print("Overview of the Data:")
         print(f"Number of Rows: {self.data.shape[0]}")
         print(f"Number of Columns: {self.data.shape[1]}")
@@ -31,7 +33,7 @@ class BrentDataAnalysis:
 
     def describe_data(self):
         """Provide basic summary statistics"""
-        print(f"{'*'*70}")
+        print(f"\n{'*'*70}")
         print(f"Basic statistical summary for numerical columns\n {self.data.describe()}")
         if self.data.dtypes[self.data.dtypes == "object"].any():
           print(f"\nBasic statistical summary for categorical columns\n {self.data.describe(include=[object, 'category'])}")
@@ -40,7 +42,7 @@ class BrentDataAnalysis:
 
     def identify_missing_values(self):
         """Identify missing values in the dataset."""
-        print(f"{'*'*70}")
+        print(f"\n{'*'*70}")
         print("Missing Values:")
         missing_values = self.data.isnull().sum()
         print(missing_values[missing_values > 0])
@@ -51,19 +53,23 @@ class BrentDataAnalysis:
         rows_before = self.data.shape[0]
         self.data.drop_duplicates(inplace=True)
         rows_after = self.data.shape[0]
-        print(f"{'*'*70}")
+        print(f"\n{'*'*70}")
         print("Duplicates removed.")
-        print(f"Shape of the data after removing duplicates. \n {self.data.shape}")
         print(f"Number of rows removed for the duplicate rows case: {rows_before - rows_after}")
+        print(f"Total rows left after duplicate rows removed. \n {self.data.shape[0]}")
         print("\n")
 
     def plot_time_series(self):
         """Plot time series data."""
+        print(f"\n{'*'*70}")
         plt.figure(figsize=(12, 6))
-        # plt.plot(self.data.index, self.data['Price'])
         sns.lineplot(data=self.data, x=self.data.index, y='Price')
         plt.title('Brent Oil Price Time Series')
         plt.xlabel('Date')
         plt.ylabel('Price')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        plt.savefig('../notebooks/plots/ctime_series_plot.png')
+        plt.show()
 
 
