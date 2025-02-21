b986236ae3637627384c2b4cd6b3bf9da1da3ca8
amare1990 <amaremek@gmail.com>
2025-02-21 20:28:01 +0300

    Update the main end-to-end processes pipeliner to run all processes of statistical modeling

--- src/src.py
+++ src/src.py
@@ -3,6 +3,12 @@
 import os
 import sys
 
+import pandas as pd
+import numpy as np
+
+import warnings
+warnings.filterwarnings('ignore')
+
 
 # Get the root directory of the project
 ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
@@ -15,17 +21,36 @@ base_dir = "/home/am/Documents/Software Development/10_Academy Training/week-10/
 
 
 from scripts.data_analysis import BrentDataAnalysis
-
+from scripts.statistical_modeling import StatisticalModel
 
 if __name__ == "__main__":
 
-  file_path = f"{base_dir}/data/BrentOilPrices.csv"
-  save_path = f"{base_dir}/data/processed_data.csv"
+  # Run the pipeline processes for the data analysis module
+  # file_path = f"{base_dir}/data/BrentOilPrices.csv"
+  # save_path = f"{base_dir}/data/processed_data.csv"
 
-  workflow = BrentDataAnalysis(file_path)
-  workflow.overview_of_data()
-  workflow.describe_data()
-  workflow.identify_missing_values()
-  workflow.remove_duplicates()
-  workflow.save_processed_data(save_path)
-  workflow.plot_time_series()
+  # workflow = BrentDataAnalysis(file_path)
+  # workflow.overview_of_data()
+  # workflow.describe_data()
+  # workflow.identify_missing_values()
+  # workflow.remove_duplicates()
+  # workflow.save_processed_data(save_path)
+  # workflow.plot_time_series()
+
+  # Run the pipeline processes for the statistical data modeling modules
+  processed_data_path = f"{base_dir}/data/processed_data.csv"
+  df_processed = pd.read_csv(processed_data_path)
+  statistical_model = StatisticalModel(df_processed)
+  result = statistical_model.check_stationarity()
+  adf_statistic = result['ADF Statistic']
+  p_value = result['p-value']
+  print(f"ADF Statistic: {adf_statistic}, \np-value: {p_value}")
+
+  statistical_model.plot_acf_pacf()
+
+  statistical_model.fit_arima()
+
+  statistical_model.fit_garch()
+
+  trace = statistical_model.bayesian_inference()
+
